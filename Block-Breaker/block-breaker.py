9e820255043ce89fc7087f42a79f95c937887693
Silas Berger <silas.berger@outlook.com>
2024-03-20 16:20:49 +0100

    Improve collision.

--- Block-Breaker/block-breaker.py
+++ Block-Breaker/block-breaker.py
@@ -1,7 +1,7 @@
 import pygame
 
 # Spiel initialisieren
-pygame.init()  
+pygame.init()
 
 # Einstellungen für das Fenster
 fenster_breite = 400
@@ -20,12 +20,12 @@ screen = pygame.display.set_mode([fenster_breite, fenster_hoehe])
 clock = pygame.time.Clock()
 
 # Einstellungen und Startposition Ball
-ball_radius = 10 
+ball_radius = 10
 ball_startposition_x = (fenster_breite / 2)
 ball_startposition_y = (fenster_hoehe / 2)
 ball = [ball_startposition_x, ball_startposition_y]
 
-ball_richtung = [-5, -10]
+ball_richtung = [-0.5, -1]
 
 # Einstellungen und Startposition Paddel
 paddel_breite = 100
@@ -40,38 +40,43 @@ paddel_bewegung_distanz = 20
 
 def aktualisiere_ball_position():
     global ball
-    
+
     # Berechne Kollisionen mit der Wand
     kollision_links = ball[0] - ball_radius <= 0
     kollision_rechts = ball[0] + ball_radius >= fenster_breite
     kollision_oben = ball[1] - ball_radius <= 0
     kollision_unten = ball[1] + ball_radius >= fenster_hoehe
-    
+
     # Berechne Kollisionen mit dem Paddel
-    kollisionsbox = [ball[0] - (ball_radius / 2), ball[1] - (ball_radius / 2), 2 * ball_radius, 2 * ball_radius]
+    ball_mitte_x = ball[0]
+    ball_mitte_y = ball[1]
+    ball_box_x = ball_mitte_x - ball_radius
+    ball_box_y = ball_mitte_y - ball_radius
+    ball_box_breite = 2 * ball_radius
+    kollisionsbox = [ball_box_x, ball_box_y, ball_box_breite, ball_box_breite]
     kollision_mit_paddel = pygame.rect.Rect(paddel).colliderect(kollisionsbox)
-    
+
     # Passe Richtung an, falls eine Kollision entedeckt wurde
     if kollision_links or kollision_rechts:
-        ball_richtung[0] = ball_richtung[0] * -1  
+        ball_richtung[0] = ball_richtung[0] * -1
     elif kollision_oben or kollision_unten or kollision_mit_paddel:
         ball_richtung[1] = ball_richtung[1] * -1
-    
+
     # Berechne neue Position des Balls gemäss der Richtung
     ball = [ball[0] + ball_richtung[0], ball[1] + ball_richtung[1]]
-    
+
 
 while True:
-    clock.tick(10)
+    clock.tick(100)
     screen.fill(schwarz)
-    
+
     pygame.draw.circle(screen, farbe_ball, ball, ball_radius)
     aktualisiere_ball_position()
-    
+
     pygame.draw.rect(screen, farbe_paddel, paddel)
-    
+
     pygame.display.flip()
-    
+
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
