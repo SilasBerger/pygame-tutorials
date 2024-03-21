f04d9164bd61e2d2bd4f7d3a66556eb09a50e04e
Silas Berger <silas.berger@outlook.com>
2024-03-21 15:36:55 +0100

    Add paddle and end condition.

--- Breakout/breakout_v2.py
+++ Breakout/breakout_v2.py
@@ -14,6 +14,10 @@ background_color = black
 window_width = 800
 window_height = 600
 
+# Sonstige einstellungen
+paddle_speed = 20
+paddle_y = window_height * 0.9
+
 # Pygame-Fenster vorbereiten
 pygame.display.set_caption("Breakout 👾")
 screen = pygame.display.set_mode([window_width, window_height])
@@ -31,19 +35,50 @@ class Ball(pygame.sprite.Sprite):
         self.rect.center = (x, y)
         self.direction = (-1, -2)
 
-    def update_position(self):
-        if self.rect.left <= 0 or self.rect.right >= window_width:
-            new_x_direction = self.direction[0] * -1
-            new_y_direction = self.direction[1]
-            self.direction = (new_x_direction, new_y_direction)
+    def hits_side_of_window(self):
+        return self.rect.left <= 0 or self.rect.right >= window_width
 
-        if self.rect.top <= 0 or self.rect.bottom >= window_height:
-            new_x_direction = self.direction[0]
-            new_y_direction = self.direction[1] * -1
-            self.direction = (new_x_direction, new_y_direction)
+    def hits_top_of_window(self):
+        return self.rect.top <= 0
 
+    def is_below_paddle(self):
+        return self.rect.center[1] > paddle_y
+
+    def stop(self):
+        self.direction = (0, 0)
+
+    def move(self):
         new_x = self.rect.center[0] + self.direction[0]
         new_y = self.rect.center[1] + self.direction[1]
+        self.rect.center = (new_x, new_y)
+
+    def flip_x_direction(self):
+        new_x_direction = self.direction[0] * -1
+        new_y_direction = self.direction[1]
+        self.direction = (new_x_direction, new_y_direction)
+
+    def flip_y_direction(self):
+        new_x_direction = self.direction[0]
+        new_y_direction = self.direction[1] * -1
+        self.direction = (new_x_direction, new_y_direction)
+
+
+class Paddle(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('sprites/49-Breakout-Tiles.png')
+        self.image = pygame.transform.scale(self.image, (150, 40))
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+
+    def left(self):
+        new_x = self.rect.center[0] - paddle_speed
+        new_y = self.rect.center[1]
+        self.rect.center = (new_x, new_y)
+
+    def right(self):
+        new_x = self.rect.center[0] + paddle_speed
+        new_y = self.rect.center[1]
         self.rect.center = (new_x, new_y)
 
 
@@ -51,15 +86,35 @@ ball = Ball(500, 300)
 balls = pygame.sprite.Group()
 balls.add(ball)
 
+paddle = Paddle(window_width / 2, paddle_y)
+paddles = pygame.sprite.Group(paddle)
 
 run = True
 while run:
     clock.tick(100)
     screen.fill(background_color)
 
-    ball.update_position()
+    ball.move()
     balls.update()
     balls.draw(screen)
+
+    paddles.update()
+    paddles.draw(screen)
+
+    if ball.hits_top_of_window():
+        ball.flip_y_direction()
+
+    if ball.hits_side_of_window():
+        ball.flip_x_direction()
+
+    if ball.is_below_paddle():
+        background_color = red
+        paddle_speed = 0
+        ball.stop()
+
+    ball_hits_paddle = pygame.sprite.spritecollide(ball, paddles, False)
+    if ball_hits_paddle:
+        ball.flip_y_direction()
 
     # Die Änderungen im Spielfenster sichtbar machen.
     pygame.display.flip()
@@ -67,3 +122,9 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                paddle.left()
+            if event.key == pygame.K_RIGHT:
+                paddle.right()
+
